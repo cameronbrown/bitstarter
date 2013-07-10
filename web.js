965f2201737676f6cae0c2998befd930f60bf7c3
Cameron Brown <cameronjbrown@gmail.com>
2013-07-10 23:27:51 +0000

    Get response from index.html

--- web.js
+++ web.js
@@ -3,10 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var indexBuf = fs.readFileSync("index.html");
+  response.send(indexBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+// vim: et sw=2 sts=2 ai
